1c59d256c2c99976537f766b60761c4b887d04c1
anasshofyan <anasshofyanmf@gmail.com>
2021-02-17 01:27:08 +0700

    Change marbot

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -3,7 +3,7 @@
     <div class="w-6/6 sm:text-left sm:w-3/6 p-2">
       <p class="text-7xl font-bold"><span class="text-ansomarfa-yellow">Anas Shofyan</span> <br>
         <span class="text-ansomarfa-dark">Martunis Fateh</span></p>
-        <p class="text-base text-ansomarfa-dark mt-7 leading-7 mb-6">I have knowledge of websites and design, then I also like to do research before making a project that can help many users later. Passionate, hard work, creative, innovative, and like to learn new things, are traits that I think reflect as an individual that can lead me to bigger goals.</p>
+        <p class="text-base text-ansomarfa-dark mt-7 leading-7 mb-10">I have knowledge of websites and design, then I also like to do research before making a project that can help many users later. Passionate, hard work, creative, innovative, and like to learn new things, are traits that I think reflect as an individual that can lead me to bigger goals.</p>
         <a href="mailto:anasshofyanmf@gmail.com?subject=I am interested in your skills" class="bg-ansomarfa-yellow text-ansomarfa-dark text-base px-4 py-3 rounded-md mt-6 hover:bg-ansomarfa-dark hover:text-ansomarfa-light mr-4">Hire Me</a>
         <a href="https://drive.google.com/file/d/1C3THTKjg6NVUDjCiQDpLzVB5115Icc-z/view?usp=sharing" target="_blank" class=" border-solid border-2 border-ansomarfa-yellow text-ansomarfa-yellow text-base px-4 py-2.5 rounded-md mt-6 hover:bg-ansomarfa-yellow hover:text-ansomarfa-dark">Download CV</a>
     </div>
